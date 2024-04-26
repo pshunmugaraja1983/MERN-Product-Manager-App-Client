25c550a0a49d7cca17abdff44046f2221f1a7cf1
pshunmugaraja1983 <pshunmugaraja1983@gmail.com>
2024-04-26 16:39:04 +0530

    Client Component

---
+++ src/components/ProductItems.js
@@ -0,0 +1,64 @@
+
+import Table from 'react-bootstrap/Table';
+
+
+const ProductItems = ({productItems}) => {
+
+    const formatDate = (dateAsString) => {
+
+        const dateObj = new Date(dateAsString);
+        const dateAsStr 
+            = dateObj.getDate() + "-" + (dateObj.getMonth() + 1) + "-" + dateObj.getFullYear();
+
+        return dateAsStr;
+    }
+
+    function productsTable() {
+        return (
+          <Table striped bordered hover>
+                  <thead>
+                      <tr>
+                      <th>#</th>
+                      <th>Name</th>
+                      <th>Description</th>
+                      <th>Amount</th>
+                      <th>Items in Stock</th>
+                      </tr>
+                  </thead>
+
+                  <tbody>
+                    {
+                        productItems.map( (productItem, index) => {
+
+                            return (
+
+                                    <tr key={productItem.id}>
+                                        <td>{index + 1}</td>
+                                        <td>{productItem.name}</td>
+                                        <td>{productItem.description}</td>
+                                        <td>{productItem.price}</td>
+                                        <td>{productItem.itemsInStock}</td>
+                                        
+                                    </tr>                           
+                                
+                            )
+                        })
+                    }                
+                </tbody>
+
+
+                  </Table>
+        );
+      }
+      
+      return (
+        <div>
+
+            <h2> Products </h2>
+            {productsTable()}            
+
+        </div>
+    );
+
+}
+export {ProductItems}
